f03124b1a3f608b4cb528f191e868439d5dda77d
emily-cloud <emilyaiai86@gmail.com>
2025-06-04 10:00:45 +0200

    add one test command

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -2,6 +2,9 @@
 //random input generation command:
 // jot -r 8000 1 10000 | awk '!seen[$0]++' | head -n 3000 | tr '\n' ' ' | tee >(wc -w) | xargs ./PmergeMe
 
+
+//./PmergeMe $(shuf -i 1-5000 -n 3000)
+
 int main(int argc, char** argv){
     if(argc < 2){
         std::cerr << "Usage: " << argv[0] << " <numbers>" << std::endl;
